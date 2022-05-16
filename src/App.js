3398393a18ff126a814e9de3a01440d41d27f717
MeganEaton <101661178+MeganEaton@users.noreply.github.com>
2022-05-16 11:15:45 -0400

    Added footer and search form w/ alert
    When form is submitted, value is displayed within the alert. Created new Dictionary css and added margin styling to all components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./App.css";
 
+import Dictionary from "./Dictionary.js";
+
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">Hello to New Dictionary App</header>
+    <div className="App container">
+      <header className="App-header">Dictionary App</header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>
+        This app was made by{" "}
+        <a href="https://www.meganeaton.me" target="_blank" rel="noreferrer">
+          Megan Eaton
+        </a>
+      </footer>
     </div>
   );
 }
